edcabf7a409449ca114dc85ccec2d9cca7326f66
Tomas Mehdi <tomymehdi@gmail.com>
2022-08-25 17:15:20 +0200

    scripts with truffle

---
+++ truffle/scripts/index.js
@@ -0,0 +1,28 @@
+// scripts/index.js
+module.exports = async function main (callback) {
+    try {
+      // Retrieve accounts from the local node
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts)
+
+      // Set up a Truffle contract, representing our deployed Box instance
+      const Box = artifacts.require('Box');
+      const box = await Box.deployed();
+
+      // Call the retrieve() function of the deployed Box contract
+      const value = await box.retrieve();
+      console.log('Box value is', value.toString());
+
+      // Send a transaction to store() a new value in the Box
+      await box.store(23);
+
+      // Call the retrieve() function of the deployed Box contract
+      const valueAfterStoring = await box.retrieve();
+      console.log('Box value is', valueAfterStoring.toString());
+  
+      callback(0);
+    } catch (error) {
+      console.error(error);
+      callback(1);
+    }
+  };
